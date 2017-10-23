89bae6a14efe2f1ea73e899e5fb938a7d7e18d42
du86796922 <86796922@qq.com>
2017-10-23 23:59:53 +0800

    注册功能

--- src/myOrderSystem/controllers/default.go
+++ src/myOrderSystem/controllers/default.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"myOrderSystem/models"
+	"github.com/astaxie/beego/logs"
 )
 
 type MainController struct {
@@ -12,4 +14,14 @@ func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+	tmp :=&models.UserInfo{
+		"yangwei123",
+		"18112188839",
+		"8821399",
+	}
+	res,err:=models.UserMesUpData("order_system","user_info",tmp)
+	if err!=""{
+		logs.Info(err)
+	}
+	logs.Info("插入状态",res)
 }
